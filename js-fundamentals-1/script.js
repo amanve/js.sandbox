559f3ebe600d0cbbe6b0a94fc66a8a861755db2d
Aman Verma <aman.kash94@gmail.com>
2022-03-03 16:05:05 +0530

    values and variables

---
+++ js-fundamentals-1/script.js
@@ -0,0 +1,21 @@
+let js = 'fun'
+// if (js === 'fun') alert('JS is fun to work with!')
+
+console.log('js')
+console.log(5 + 5 - 2 * 3)
+
+let firstName = 'Aman'
+/* 
+Naming conventions - "$" "_" "alphabet"
+no symbols like let 5name = 3
+or let @name = "aman"
+or reserved keywords like "new" "function"
+let new="fun" => this is prohibited
+let first&name = "Aman" Symbol is prohibited
+
+Best Naming Convention is using CAMEL CASE
+*/
+
+let PI = 3.1419
+console.log(PI)
+console.log(Math.PI) /* Both shows different values */
